ec26a038dc801ac2d6d59c7c2c2b1fe19a0510c2
Prachip9767 <prachip293@gmail.com>
2021-10-18 16:08:23 +0530

    Evaluation c1 unit 5

--- app/src/main/java/com/example/myapplication/UI/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/UI/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.myapplication.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private
+    private lateinit var pagingViewModel: PagingViewModel
+    private lateinit var charecterAdapter: CharecterAdapter
+    private lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mainBinding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        pagingViewModel=ViewModelProvider(this).get(PagingViewModel::class.java)
+        pagingViewModel.getPage().
     }
 }
